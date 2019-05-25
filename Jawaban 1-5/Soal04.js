dd6561c7cb2ae2f45a5e6ab285194a4d842c329d
Sahrul Sidik <51010863+SSsahrulsidik@users.noreply.github.com>
2019-05-25 20:41:50 +0700

    Update Soal04.js

--- Jawaban 1-5/Soal04.js
+++ Jawaban 1-5/Soal04.js
@@ -1,4 +1,5 @@
 // Soal 4
+// Belum Selesai
 
 // Buatlah function untuk mencetak gambar seperti dibawah ini, yang mempunyai sebuah parameter sebagai panjang atau lebar gambar. Parameter harus merupakan bilangan ganjil.
 // Catatan: Harus menggunakan perulangan jangan hardcode/ditulis langsung.
