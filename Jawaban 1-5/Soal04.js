e4a5769b8826664fa4c690209531c083ca4d139f
Sahrul Sidik <51010863+SSsahrulsidik@users.noreply.github.com>
2019-05-25 20:41:23 +0700

    Update Soal04.js

--- Jawaban 1-5/Soal04.js
+++ Jawaban 1-5/Soal04.js
@@ -3,8 +3,8 @@
 // Buatlah function untuk mencetak gambar seperti dibawah ini, yang mempunyai sebuah parameter sebagai panjang atau lebar gambar. Parameter harus merupakan bilangan ganjil.
 // Catatan: Harus menggunakan perulangan jangan hardcode/ditulis langsung.
 
+// coba-coba
 let t = '';
-
 for(lebar = 1; lebar <= 4; lebar++){
   if(lebar % 2 == 0){
     t += '';
@@ -17,20 +17,16 @@ for(lebar = 1; lebar <= 4; lebar++){
   t += '\n';
 }
 console.log(t);
-//  1 2 3 4 5 
-// 1 2 3 4 5 
-//  1 2 3 4 5 
-// 1 2 3 4 5 
-//  1 2 3 4 5 
+//  # # # # # 
+// # # # # # 
+//  # # # # # 
+// # # # # # 
 
+//
 let s ='';
 let panjang1 = '=';
 let lebar1 = '*';
-
 for(i = 1; i <= 5; i++){
-    // jika j =0 j kurang dari 5 j + 1 maa dia akan mengulang dari indek ke 0
-    // jika j = 1 apakah kurang dari 5 iya kurang dari 5 j ditambah dengan index selanjutnya yaitu 1 berarti akan meunculkan nilai satu
-    // jika j = 2 atau jika id mulainya dari 2 maka akan meloop dari index 0 dengan nilai 2
     for(j = 1; j <= 5; j++){
         // s += '=';
         if('a') {
@@ -40,7 +36,11 @@ for(i = 1; i <= 5; i++){
     s += '\n';
 }
 console.log(s);
-
+// =====
+// =====
+// =====
+// =====
+// =====
 
 
 
